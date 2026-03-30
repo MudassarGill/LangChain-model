fd1f20059dbd0463eb95d235da26f5beed221331
MudassarGill <mudassarhussain6533@gmail.com>
2026-03-29 21:28:06 -0700

    local model added

--- ChatModels/chatmodel_hf.py
+++ ChatModels/chatmodel_hf.py
@@ -1,18 +1,15 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
-import os 
-import numpy as np 
-
 
 load_dotenv()
 
-model=HuggingFaceEndpoint(
-    repo_id="google/flan-t5-base",
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
     task="text-generation",
 )
 
-model=ChatHuggingFace(llm=model)
+model = ChatHuggingFace(llm=llm)
 
-response=model.invoke("What is the capital of Pakistan?")
+response = model.invoke("What is the capital of Pakistan?")
 
-print(response)
+print(response.content)
---
+++ ChatModels/chatmodel_local.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
+from dotenv import load_dotenv
+import os 
+import numpy as np 
+
+load_dotenv()
+llm=HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        
+        "temperature": 0.5,
+        "max_new_tokens": 50,
+        
+    }
+)
+
+
+model=ChatHuggingFace(llm=llm)
+
+response=model.invoke("What is the capital of Pakistan?")
+
+print(response)

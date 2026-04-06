d51c7c19b169c3ecc0c640a305c3a42e0ef11964
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-05 19:59:07 -0700

    ouput parser

--- ChatModels/models_langchain.py
+++ ChatModels/models_langchain.py
@@ -1,18 +1,19 @@
 from langchain_huggingface import ChatHuggingFace
-from langchain.promot import PromptTemplate
-from langchain.parse import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from pydantic import Basemodel
-from typing import Field,Optional
-import pandas as pd 
-import numpy as np 
-import os 
-import sys
+
 
 
 load_dotenv()
 
-model=ChatHuggingFace('prism-ml/Bonsai-8B-gguf')
+model=ChatHuggingFace.from_model_id(
+    model_id='prism-ml/Bonsai-8B-gguf',
+    task="text-generation",
+    pipeline_kwargs={
+        "temperature": 0.5,
+    }
+)
 promot=PromptTemplate(
       input_variables=["job"],
       template="Write a job application email for {job}",

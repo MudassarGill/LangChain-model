4dc23ce69bb177adbf3ef0bb0c964743b6612c6a
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-05 22:07:13 -0700

    system massage and human massage added

--- ChatModels/chatmodel.py
+++ ChatModels/chatmodel.py
@@ -1,12 +1,17 @@
 from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage,SystemMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxx"
 
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0,max_completion_tokens=10)
 
-response = model.invoke("What is the capital of Pakistan")
+massage=[
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content="What is the capital of Pakistan")
+]
+
+response=model.invoke(massage)
 
 print(response.content)

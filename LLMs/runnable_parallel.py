cdbf29f6e2fd6fb253bf6ddb1e0abf93ea2ef1b5
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-06 19:15:35 -0700

    runnable parallel with chain

--- LLMs/runnable_parallel.py
+++ LLMs/runnable_parallel.py
@@ -1,7 +1,8 @@
+from langchain_core.runnables import RunnableSequence
 from langchain_huggingface import HuggingFacePipeline,ChatHuggingFace,HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableSequence
+from langchain_core.runnables import RunnableParallel
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,24 +10,29 @@ load_dotenv()
 model=HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
-   
+    
 )
 
 prompt1=PromptTemplate(
-    template='write a joke on {topic}',
+    template='Genrate a tweet on {topic}',
     input_variables=["topic"]
 )
 
 prompt2=PromptTemplate(
-    template='write a detailed summmary of the following text \n {text}',
-    input_variables=["text"]
+    template='Genrate a post for Linkedin on {topic}',
+    input_variables=["topic"]
 )
 
 
 
 parser=StrOutputParser()
 
+parallel_chain=RunnableParallel(
+     {
+        'tweet':RunnableSequence(prompt1,model,parser),
+        'linkedin':RunnableSequence(prompt2,model,parser)
+     }
+)
 
-chain=RunnableSequence(prompt1,model,parser,prompt2,model,parser)
-result =chain.invoke({'topic': 'AI'})
+result=parallel_chain.invoke({'topic': 'AI'})
 print(result)

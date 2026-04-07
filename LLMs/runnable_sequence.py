ad731bc510ed324121ad8b07ab69a436cf159fe7
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-06 19:04:19 -0700

    add two prompt to test runnable sequence with chain

--- LLMs/runnable_sequence.py
+++ LLMs/runnable_sequence.py
@@ -1,6 +1,6 @@
 from langchain_huggingface import HuggingFacePipeline,ChatHuggingFace,HuggingFaceEndpoint
-from langchain.core.prompts import PromptTemplate
-from langchain.core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain.schema.runnable import RunnableSequence
 from dotenv import load_dotenv
 
@@ -14,14 +14,21 @@ model=HuggingFacePipeline.from_model_id(
     }
 )
 
-prompt=PromptTemplate(
+prompt1=PromptTemplate(
     template='write a joke on {topic}',
     input_variables=["topic"]
 )
 
+prompt2=PromptTemplate(
+    template='write a detailed summmary of the following text \n {text}',
+    input_variables=["text"]
+)
+
+
+
 parser=StrOutputParser()
 
 
-chain=RunnableSequence(prompt,model,parser)
+chain=RunnableSequence(prompt1,model,parser,prompt2,model,parser)
 result =chain.invoke({'topic': 'AI'})
 print(result)
